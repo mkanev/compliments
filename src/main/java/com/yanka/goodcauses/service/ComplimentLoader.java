3b7bf49bd9790fdbec6dff843e6f4c9573553804
Maksim.Kanev <maksim.kanev@gmail.com>
2014-03-03 16:36:09 +0400

    Deployment ready

--- src/main/java/com/yanka/goodcauses/service/ComplimentLoader.java
+++ src/main/java/com/yanka/goodcauses/service/ComplimentLoader.java
@@ -11,6 +11,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -19,6 +20,7 @@ import org.springframework.util.StreamUtils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,6 +44,7 @@ public class ComplimentLoader extends LoggedClass {
             request.setHeader("User-Agent", USER_AGENT);
             request.setHeader("Accept", "*/*");
             request.setHeader("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4");
+            request.setHeader("Accept-Charset", "utf-8");
             request.setHeader("Connection", "keep-alive");
             request.setHeader("Referer", "http://online-generators.ru/compliments");
             request.setHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
@@ -53,13 +56,7 @@ public class ComplimentLoader extends LoggedClass {
             request.setEntity(new UrlEncodedFormEntity(postParams));
 
             HttpResponse response = httpClient.execute(request);
-            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            StringBuilder result = new StringBuilder();
-            String line;
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-            String msg = result.toString();
+            String msg = EntityUtils.toString(response.getEntity());
             if (StringUtils.isBlank(msg)) {
                 return;
             }

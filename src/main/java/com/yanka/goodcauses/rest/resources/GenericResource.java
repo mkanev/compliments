01cc2ea614d1e30e2ad9c28ef1b5d7f2e0cd4227
Maksim.Kanev <maksim.kanev@gmail.com>
2014-03-04 20:25:32 +0400

    User excluded

--- src/main/java/com/yanka/goodcauses/rest/resources/GenericResource.java
+++ src/main/java/com/yanka/goodcauses/rest/resources/GenericResource.java
@@ -5,34 +5,9 @@ import com.yanka.goodcauses.common.LoggedClass;
 import com.yanka.goodcauses.model.BaseDBObject;
 import com.yanka.goodcauses.service.GenericManager;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.support.MutableSortDefinition;
-import org.springframework.beans.support.PagedListHolder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 
 /**
  * @author <a href="mailto:maksim.kanev@waveaccess.ru">Maksim Kanev</a>
@@ -47,7 +22,7 @@ public abstract class GenericResource<T extends BaseDBObject, PK extends Long> e
         this.genericManager = genericManager;
     }
 
-    @POST
+    /*@POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public T create(T entity) {
@@ -82,13 +57,13 @@ public abstract class GenericResource<T extends BaseDBObject, PK extends Long> e
     public void delete(@PathParam("id") PK id) {
         logDebug("delete(id)");
         this.genericManager.remove(id);
-    }
+    }*/
 
     protected ObjectMapper getMapper() {
         return mapper;
     }
 
-    protected boolean isAdmin() {
+    /*protected boolean isAdmin() {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Object principal = authentication.getPrincipal();
@@ -105,14 +80,14 @@ public abstract class GenericResource<T extends BaseDBObject, PK extends Long> e
         }
 
         return false;
-    }
+    }*/
 
     protected ObjectWriter getPreviewWriter() {
-        return this.isAdmin() ? getMapper().writerWithView(JsonViews.Admin.class) : getMapper().writerWithView(JsonViews.Preview.class);
+        return /*this.isAdmin() ? getMapper().writerWithView(JsonViews.Admin.class) :*/ getMapper().writerWithView(JsonViews.Preview.class);
     }
 
-    protected ObjectWriter getExtendedViewWriter() {
+    /*protected ObjectWriter getExtendedViewWriter() {
         return this.isAdmin() ? getMapper().writerWithView(JsonViews.Admin.class) : getMapper().writerWithView(JsonViews.Extended.class);
-    }
+    }*/
 
 }
